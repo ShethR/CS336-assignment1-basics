4d844dea284db1f9d5d4882025009109dd1b0528
Jugal sheth <jugalsheth2001@gmail.com>
2025-11-22 19:59:17 -0500

    added bpe pretokenizer

--- tests/adapters.py
+++ tests/adapters.py
@@ -8,6 +8,7 @@ import numpy.typing as npt
 import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
+import cs336_basics.bpe as bpe
 
 
 def run_linear(
@@ -452,7 +453,9 @@ def run_cross_entropy(
     raise NotImplementedError
 
 
-def run_gradient_clipping(parameters: Iterable[torch.nn.Parameter], max_l2_norm: float) -> None:
+def run_gradient_clipping(
+    parameters: Iterable[torch.nn.Parameter], max_l2_norm: float
+) -> None:
     """Given a set of parameters, clip their combined gradients to have l2 norm at most max_l2_norm.
 
     Args:
@@ -589,4 +592,5 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+    vocab_count = bpe.pretokenize(input_path, special_tokens)
+    return ()
